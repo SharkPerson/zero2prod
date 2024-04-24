1089ab0aea645d1248f7195b207243116f093559
JW <jombbey@gmail.com>
2024-04-24 22:35:42 +1000

    Remove unused import

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use actix_web::dev::Server;
-use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
 async fn greet(req: HttpRequest) -> impl Responder {
