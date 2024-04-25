84c9fbb5da5ab78bdd457a0fc25fec3a91b96670
JW <jombbey@gmail.com>
2024-04-25 11:35:05 +1000

    Add functionality and tests for subscription route

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,20 +1,65 @@
 use std::net::TcpListener;
+
 // Can use cargo expand --test health_check
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
-
     let address: String = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get(&format!("{}/health_check", address))
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute target");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length()); // check the body is empty
+}
+
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    // POST HTTP request at /subscriptions
+    // Return 200 status code
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let valid_body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(valid_body)
+        .send()
+        .await
+        .expect("failed to execute target");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_when_data_is_missing() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both the name and email"),
+    ];
+
+    for (invalid_body, desc) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("failed to execute target");
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with a bad request when the payload was {}",
+            desc
+        );
+    }
 }
 
 fn spawn_app() -> String {
