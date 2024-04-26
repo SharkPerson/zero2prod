0933c31afbbc10af1361974c829a1eb76b096d2b
JW <jombbey@gmail.com>
2024-04-26 15:59:14 +1000

    Fix test isolation issue in unit tests by randomising a database on each test function

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,8 @@
 use std::net::TcpListener;
-use zero2prod::startup::run;
+use uuid::Uuid;
+use zero2prod::{configuration::DatabaseSettings, startup::run};
 
-use sqlx::PgPool;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use zero2prod::configuration::get_configuration;
 
 #[allow(dead_code)]
@@ -74,16 +75,35 @@ async fn subscribe_returns_400_when_data_is_missing() {
 }
 
 async fn spawn_app() -> TestApp {
-    let configuration = get_configuration().expect("Failed to read config");
-    let connection_string = configuration.database.connection_string();
-    let db_pool = PgPool::connect(&connection_string)
-        .await
-        .expect("Failed to connect to Postgres");
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
+    let address = format!("http://127.0.0.1:{}", port);
+
+    let mut configuration = get_configuration().expect("Failed to read config");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+    let db_pool = configure_database(&configuration.database).await;
     let server = run(listener, db_pool.clone()).expect("Failed to bind address");
     // Launch the server as a background task
     let _ = tokio::spawn(server);
-    let address = format!("http://127.0.0.1:{}", port);
     TestApp { address, db_pool }
+}
+
+async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database");
+
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
 }
